2874e2d4e382b4278acf22ad2e00902e50d57c2f
yasinthanvickneswaran <147663006+yasinthanvickneswaran@users.noreply.github.com>
2024-06-04 15:48:03 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -9,6 +9,8 @@ python -m cect.UpdatedSpreadsheetDataReader
 python -m cect.UpdatedSpreadsheetDataReader2                                                                                                                                                                  
 #python -m cect.OpenWormReader                                                                                                                                                            
 python -m cect.VarshneyDataReader
+python -m cect.Cook2019DataReader
+python -m cect.Cook2019HermReader
 
 
 
