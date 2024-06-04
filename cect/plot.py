20054058714065e31bd9b56e9d4c5857002c4330
yasinthanvickneswaran <v.yasinthan@outlook.com>
2024-06-04 18:38:04 +0100

    witvliet_2020_8

--- cect/plot.py
+++ cect/plot.py
@@ -4,5 +4,5 @@ import plotly.graph_objects as go
 df = px.data.iris()
 fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species",
                  size='petal_length', hover_data=['petal_width'])
-with open("cect\data\witvliet_2020_7.json", "w") as f:
+with open("cect\data\witvliet_2020_8.json", "w") as f:
     f.write(fig.to_json())
