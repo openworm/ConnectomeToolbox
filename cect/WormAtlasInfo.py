856d9e9fc1c144cc55cbb0d5b33905116a79beab
pgleeson <p.gleeson@gmail.com>
2024-08-05 12:40:09 +0100

    Adding color chart from WormAtlas & initila page for all cells...

---
+++ cect/WormAtlasInfo.py
@@ -0,0 +1,123 @@
+# Data taken from https://www.wormatlas.org/colorcode.htm
+
+WA_COLORS = {}
+
+WA_COLORS["Hermaphrodite"] = {}
+
+
+# MUSCLE
+WA_COLORS["Hermaphrodite"]["Muscle"] = {}
+WA_COLORS["Hermaphrodite"]["Muscle"]["intestinal muscle"] = "#006600"
+WA_COLORS["Hermaphrodite"]["Muscle"]["vulval muscle"] = "#99c66"
+WA_COLORS["Hermaphrodite"]["Muscle"]["body wall muscle"] = "#336600"
+WA_COLORS["Hermaphrodite"]["Muscle"]["uterine muscle"] = "#99cc00"
+WA_COLORS["Hermaphrodite"]["Muscle"]["sphincter and anal depressor muscle"] = "#669900"
+WA_COLORS["Hermaphrodite"]["Muscle"]["odd numbered pharyngeal muscle"] = "#33cC00"
+WA_COLORS["Hermaphrodite"]["Muscle"]["even numbered pharyngeal muscle"] = "#ccf199"
+
+# ALIMENTARY SYSTEM
+WA_COLORS["Hermaphrodite"]["Alimentary System"] = {}
+WA_COLORS["Hermaphrodite"]["Alimentary System"][
+    "marginal cells (mc) of the pharynx"
+] = "#cc33cc"
+WA_COLORS["Hermaphrodite"]["Alimentary System"]["intestinal cells"] = "#ffccff"
+
+# REPRODUCTIVE SYSTEM
+WA_COLORS["Hermaphrodite"]["Reproductive System"] = {}
+WA_COLORS["Hermaphrodite"]["Reproductive System"]["vulval epithelium"] = "#9999ff"
+WA_COLORS["Hermaphrodite"]["Reproductive System"]["germline"] = "#003366"
+WA_COLORS["Hermaphrodite"]["Reproductive System"]["DTC and somatic gonad"] = "#9966ff"
+WA_COLORS["Hermaphrodite"]["Reproductive System"]["embryo"] = "#6699cc"
+WA_COLORS["Hermaphrodite"]["Reproductive System"]["uterus"] = "#99cccc"
+WA_COLORS["Hermaphrodite"]["Reproductive System"]["spermatheca"] = "#3399ff"
+WA_COLORS["Hermaphrodite"]["Reproductive System"][
+    "spermatheca-uterine valve"
+] = "#666699"
+
+# NERVOUS TISSUE
+WA_COLORS["Hermaphrodite"]["Nervous Tissue"] = {}
+WA_COLORS["Hermaphrodite"]["Nervous Tissue"]["interneuron"] = "#ff3300"
+WA_COLORS["Hermaphrodite"]["Nervous Tissue"][
+    "General code for neuronal tissue if subtype is unspecified"
+] = "#ff3300"
+WA_COLORS["Hermaphrodite"]["Nervous Tissue"]["motor neuron"] = "#9966cc"
+WA_COLORS["Hermaphrodite"]["Nervous Tissue"]["sensory neuron"] = "#ff66cc"
+WA_COLORS["Hermaphrodite"]["Nervous Tissue"]["polymodal neuron"] = "#cc0033"
+WA_COLORS["Hermaphrodite"]["Nervous Tissue"]["neuron with unknown function"] = "#990033"
+
+
+# EXCRETORY SYSTEM
+WA_COLORS["Hermaphrodite"]["Excretory System"] = {}
+WA_COLORS["Hermaphrodite"]["Excretory System"]["excretory pore cell"] = "#ecf781"
+WA_COLORS["Hermaphrodite"]["Excretory System"]["duct cell"] = "#сс9966"
+WA_COLORS["Hermaphrodite"]["Excretory System"]["gland cell"] = "#9999cc"
+WA_COLORS["Hermaphrodite"]["Excretory System"]["excretory cell"] = "#cc3366"
+WA_COLORS["Hermaphrodite"]["Excretory System"]["excretory duct"] = "#cccc99"
+
+
+# EPITHELIAL TISSUE
+WA_COLORS["Hermaphrodite"]["Epithelial Tissue"] = {}
+WA_COLORS["Hermaphrodite"]["Epithelial Tissue"]["hypodermis"] = "#dcc3ac"
+WA_COLORS["Hermaphrodite"]["Epithelial Tissue"]["seam cell"] = "#сс6633"
+WA_COLORS["Hermaphrodite"]["Epithelial Tissue"]["socket cel, XXX cells"] = "#ff9999"
+WA_COLORS["Hermaphrodite"]["Epithelial Tissue"][
+    "sheath cel other than amphid sheath and phasmid"
+] = "#339999"
+WA_COLORS["Hermaphrodite"]["Epithelial Tissue"][
+    "amphid sheath and phasmid sheath"
+] = "#01fecc"
+WA_COLORS["Hermaphrodite"]["Epithelial Tissue"][
+    "pharyngeal-intestinal vavle, intestinal rectal valve "
+] = "#996633"
+WA_COLORS["Hermaphrodite"]["Epithelial Tissue"]["arcade cell"] = "#cccc00"
+WA_COLORS["Hermaphrodite"]["Epithelial Tissue"]["pharyngeal epithelium"] = "#996699"
+WA_COLORS["Hermaphrodite"]["Epithelial Tissue"][
+    "rectal epithelium (U, F, K, K', Y, B)"
+] = "#996666"
+WA_COLORS["Hermaphrodite"]["Epithelial Tissue"][
+    "rectal gland, pharyngeal glands"
+] = "#ccccff"
+
+
+# OTHER TISSUES
+WA_COLORS["Hermaphrodite"]["Other Tissues"] = {}
+WA_COLORS["Hermaphrodite"]["Other Tissues"]["pseudocoelomic space"] = "#ffffcc"
+WA_COLORS["Hermaphrodite"]["Other Tissues"]["basement membrane"] = "#ff9900"
+WA_COLORS["Hermaphrodite"]["Other Tissues"]["gir cell"] = "#cc9900"
+WA_COLORS["Hermaphrodite"]["Other Tissues"]["coeloemocyte"] = "#ffcc00"
+WA_COLORS["Hermaphrodite"]["Other Tissues"]["head mesodermal cell"] = "#ff6633"
+
+
+WA_COLORS["Male"] = {}
+
+# MUSCLE
+WA_COLORS["Male"]["Muscle"] = {}
+WA_COLORS["Male"]["Muscle"]["diagonal muscles"] = "#66cc66"
+WA_COLORS["Male"]["Muscle"]["anterior outer longitudinal muscles"] = "#666633"
+WA_COLORS["Male"]["Muscle"]["posterior outer longitudinal muscles"] = "#999933"
+WA_COLORS["Male"]["Muscle"]["anterior niner longitudinal muscles"] = "#99cc33"
+WA_COLORS["Male"]["Muscle"]["posterior niner longitudinal muscles"] = "#ccff33"
+WA_COLORS["Male"]["Muscle"]["caudal inner longitudinal muscles"] = "#99cc99"
+WA_COLORS["Male"]["Muscle"]["spicule retractor muscles"] = "#669966"
+WA_COLORS["Male"]["Muscle"]["spicule protractor muscles"] = "#99ff99"
+WA_COLORS["Male"]["Muscle"]["gubernacular retractor muscles"] = "#99ffcc"
+WA_COLORS["Male"]["Muscle"]["gubernacular erector muscles"] = "#66ff33"
+WA_COLORS["Male"]["Muscle"]["anterior oblique muscles"] = "#33cc99"
+WA_COLORS["Male"]["Muscle"]["posterior oblique muscles"] = "#66cc99"
+
+
+# REPRODUCTIVE SYSTEM
+WA_COLORS["Male"]["Reproductive System"] = {}
+WA_COLORS["Male"]["Reproductive System"]["vas deferens"] = "#66cccc"
+WA_COLORS["Male"]["Reproductive System"]["seminal vesicle"] = "#6699ff"
+WA_COLORS["Male"]["Reproductive System"]["proctodeum"] = "#996666"
+
+# EPITHELIAL TISSUE
+WA_COLORS["Male"]["Epithelial Tissue"] = {}
+WA_COLORS["Male"]["Epithelial Tissue"][
+    "sheath cell (spicules, hook or post-cloacal sensilla)"
+] = "#339999"
+WA_COLORS["Male"]["Epithelial Tissue"][
+    "socket cell (spicules, hook or post-cloacal sensilla)"
+] = "#ff9999"
+WA_COLORS["Male"]["Epithelial Tissue"]["ray structural cell"] = "#006666"
---
+++ docs/Cells.md
@@ -0,0 +1,94 @@
+
+## Hermaphrodite
+
+### Muscle
+
+- ![#006600](https://via.placeholder.com/15/006600/006600.png) intestinal muscle
+- ![#99c66](https://via.placeholder.com/15/99c66/99c66.png) vulval muscle
+- ![#336600](https://via.placeholder.com/15/336600/336600.png) body wall muscle
+- ![#99cc00](https://via.placeholder.com/15/99cc00/99cc00.png) uterine muscle
+- ![#669900](https://via.placeholder.com/15/669900/669900.png) sphincter and anal depressor muscle
+- ![#33cC00](https://via.placeholder.com/15/33cC00/33cC00.png) odd numbered pharyngeal muscle
+- ![#ccf199](https://via.placeholder.com/15/ccf199/ccf199.png) even numbered pharyngeal muscle
+
+### Alimentary System
+
+- ![#cc33cc](https://via.placeholder.com/15/cc33cc/cc33cc.png) marginal cells (mc) of the pharynx
+- ![#ffccff](https://via.placeholder.com/15/ffccff/ffccff.png) intestinal cells
+
+### Reproductive System
+
+- ![#9999ff](https://via.placeholder.com/15/9999ff/9999ff.png) vulval epithelium
+- ![#003366](https://via.placeholder.com/15/003366/003366.png) germline
+- ![#9966ff](https://via.placeholder.com/15/9966ff/9966ff.png) DTC and somatic gonad
+- ![#6699cc](https://via.placeholder.com/15/6699cc/6699cc.png) embryo
+- ![#99cccc](https://via.placeholder.com/15/99cccc/99cccc.png) uterus
+- ![#3399ff](https://via.placeholder.com/15/3399ff/3399ff.png) spermatheca
+- ![#666699](https://via.placeholder.com/15/666699/666699.png) spermatheca-uterine valve
+
+### Nervous Tissue
+
+- ![#ff3300](https://via.placeholder.com/15/ff3300/ff3300.png) interneuron
+- ![#ff3300](https://via.placeholder.com/15/ff3300/ff3300.png) General code for neuronal tissue if subtype is unspecified
+- ![#9966cc](https://via.placeholder.com/15/9966cc/9966cc.png) motor neuron
+- ![#ff66cc](https://via.placeholder.com/15/ff66cc/ff66cc.png) sensory neuron
+- ![#cc0033](https://via.placeholder.com/15/cc0033/cc0033.png) polymodal neuron
+- ![#990033](https://via.placeholder.com/15/990033/990033.png) neuron with unknown function
+
+### Excretory System
+
+- ![#ecf781](https://via.placeholder.com/15/ecf781/ecf781.png) excretory pore cell
+- ![#сс9966](https://via.placeholder.com/15/сс9966/сс9966.png) duct cell
+- ![#9999cc](https://via.placeholder.com/15/9999cc/9999cc.png) gland cell
+- ![#cc3366](https://via.placeholder.com/15/cc3366/cc3366.png) excretory cell
+- ![#cccc99](https://via.placeholder.com/15/cccc99/cccc99.png) excretory duct
+
+### Epithelial Tissue
+
+- ![#dcc3ac](https://via.placeholder.com/15/dcc3ac/dcc3ac.png) hypodermis
+- ![#сс6633](https://via.placeholder.com/15/сс6633/сс6633.png) seam cell
+- ![#ff9999](https://via.placeholder.com/15/ff9999/ff9999.png) socket cel, XXX cells
+- ![#339999](https://via.placeholder.com/15/339999/339999.png) sheath cel other than amphid sheath and phasmid
+- ![#01fecc](https://via.placeholder.com/15/01fecc/01fecc.png) amphid sheath and phasmid sheath
+- ![#996633](https://via.placeholder.com/15/996633/996633.png) pharyngeal-intestinal vavle, intestinal rectal valve 
+- ![#cccc00](https://via.placeholder.com/15/cccc00/cccc00.png) arcade cell
+- ![#996699](https://via.placeholder.com/15/996699/996699.png) pharyngeal epithelium
+- ![#996666](https://via.placeholder.com/15/996666/996666.png) rectal epithelium (U, F, K, K', Y, B)
+- ![#ccccff](https://via.placeholder.com/15/ccccff/ccccff.png) rectal gland, pharyngeal glands
+
+### Other Tissues
+
+- ![#ffffcc](https://via.placeholder.com/15/ffffcc/ffffcc.png) pseudocoelomic space
+- ![#ff9900](https://via.placeholder.com/15/ff9900/ff9900.png) basement membrane
+- ![#cc9900](https://via.placeholder.com/15/cc9900/cc9900.png) gir cell
+- ![#ffcc00](https://via.placeholder.com/15/ffcc00/ffcc00.png) coeloemocyte
+- ![#ff6633](https://via.placeholder.com/15/ff6633/ff6633.png) head mesodermal cell
+
+## Male
+
+### Muscle
+
+- ![#66cc66](https://via.placeholder.com/15/66cc66/66cc66.png) diagonal muscles
+- ![#666633](https://via.placeholder.com/15/666633/666633.png) anterior outer longitudinal muscles
+- ![#999933](https://via.placeholder.com/15/999933/999933.png) posterior outer longitudinal muscles
+- ![#99cc33](https://via.placeholder.com/15/99cc33/99cc33.png) anterior niner longitudinal muscles
+- ![#ccff33](https://via.placeholder.com/15/ccff33/ccff33.png) posterior niner longitudinal muscles
+- ![#99cc99](https://via.placeholder.com/15/99cc99/99cc99.png) caudal inner longitudinal muscles
+- ![#669966](https://via.placeholder.com/15/669966/669966.png) spicule retractor muscles
+- ![#99ff99](https://via.placeholder.com/15/99ff99/99ff99.png) spicule protractor muscles
+- ![#99ffcc](https://via.placeholder.com/15/99ffcc/99ffcc.png) gubernacular retractor muscles
+- ![#66ff33](https://via.placeholder.com/15/66ff33/66ff33.png) gubernacular erector muscles
+- ![#33cc99](https://via.placeholder.com/15/33cc99/33cc99.png) anterior oblique muscles
+- ![#66cc99](https://via.placeholder.com/15/66cc99/66cc99.png) posterior oblique muscles
+
+### Reproductive System
+
+- ![#66cccc](https://via.placeholder.com/15/66cccc/66cccc.png) vas deferens
+- ![#6699ff](https://via.placeholder.com/15/6699ff/6699ff.png) seminal vesicle
+- ![#996666](https://via.placeholder.com/15/996666/996666.png) proctodeum
+
+### Epithelial Tissue
+
+- ![#339999](https://via.placeholder.com/15/339999/339999.png) sheath cell (spicules, hook or post-cloacal sensilla)
+- ![#ff9999](https://via.placeholder.com/15/ff9999/ff9999.png) socket cell (spicules, hook or post-cloacal sensilla)
+- ![#006666](https://via.placeholder.com/15/006666/006666.png) ray structural cell
