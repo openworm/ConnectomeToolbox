6f136564685b29cc79bfc3009f1e9a026b6ad96f
pgleeson <p.gleeson@gmail.com>
2025-02-26 17:41:19 +0000

    Better ability to view raw symm matrices

--- cect/Analysis.py
+++ cect/Analysis.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from cect.Cells import get_contralateral_cell
 from cect.ConnectomeReader import SYMMETRY_COLORMAP
+from cect import print_
 
 
 def array_info(conn_array):
@@ -48,7 +49,7 @@ def convert_to_symmetry_array(cds, synclasses):
                             new_conn_array[i, j] or conns_cs[i, j] > 0
                         )
 
-    print("Amalgamated array:")
+    print_("Symm array:")
     array_info(new_conn_array)
 
     conn_count = 0
@@ -66,14 +67,27 @@ def convert_to_symmetry_array(cds, synclasses):
             if w != 0:
                 print(f"Connection {conn_count}:\t{pre}->{post} ({w})")
                 assert pre is not post
-                w_ = new_conn_array[cds.nodes.index(pre_), cds.nodes.index(post_)]
-                print(f"   - Mirror:\t{pre_}->{post_} ({w_})\n")
+                if pre_ not in cds.nodes:
+                    print(
+                        f"   - Mirror conn:\t{pre_}->{post_} not possible as {pre_} missing!\n"
+                    )
+                    w_ = -1
+                elif post_ not in cds.nodes:
+                    print(
+                        f"   - Mirror conn:\t{pre_}->{post_} not possible as {post_} missing!\n"
+                    )
+                    w_ = -1
+                else:
+                    w_ = new_conn_array[cds.nodes.index(pre_), cds.nodes.index(post_)]
+                    print(f"   - Mirror:\t{pre_}->{post_} ({w_})\n")
+
                 symm_conn_count += w_
-                if w_ == 0:
+                if w_ <= 0:
                     scaled_conn_array[i][j] = 0.5
-                    scaled_conn_array[
-                        cds.nodes.index(pre_), cds.nodes.index(post_)
-                    ] = -1
+                    if w_ == 0:
+                        scaled_conn_array[
+                            cds.nodes.index(pre_), cds.nodes.index(post_)
+                        ] = -1
                 conn_count += 1
 
     info = f"Of {(len(new_conn_array) ** 2)} possible edges, {conn_count} are connected, {int(symm_conn_count)} are mirrored - {'%.2f' % (100 * symm_conn_count / conn_count)}% "
@@ -90,10 +104,12 @@ def test_bilaterals():
 
     #
     from cect.White_whole import get_instance
-    # from cect.TestDataReader import get_instance
-    # from cect.Cook2020DataReader import get_instance
+    from cect.WormNeuroAtlasFuncReader import get_instance
 
-    """from cect.WormNeuroAtlasFuncReader import get_instance
+    # from cect.Cook2020DataReader import get_instance
+    # from cect.TestDataReader import get_instance
+
+    """
     from cect.RipollSanchezDataReader import get_instance
     from cect.WitvlietDataReader1 import get_instance # 65
     from cect.WitvlietDataReader2 import get_instance # 70.85
@@ -105,19 +121,19 @@ def test_bilaterals():
     from cect.WitvlietDataReader8 import get_instance # 69.70
 
     from cect.Cook2019MaleReader import get_instance
-    from cect.Cook2019HermReader import get_instance
     from cect.WitvlietDataReader8 import get_instance
     from cect.SpreadsheetDataReader import get_instance
     from cect.BrittinDataReader import get_instance
     from cect.WormNeuroAtlasMAReader import get_instance
     from cect.RipollSanchezDataReader import get_instance
+    from cect.Cook2019HermReader import get_instance
     from cect.ConnectomeView import SOCIAL_VIEW as view"""
     #
     # from cect.ConnectomeView import NEURONS_VIEW as view
 
     # from cect.ConnectomeView import NONPHARYNGEAL_NEURONS_HM_VIEW as view
-    # from cect.ConnectomeView import RAW_VIEW as view
-    from cect.ConnectomeView import PHARYNX_VIEW as view
+    from cect.ConnectomeView import RAW_VIEW as view
+    # from cect.ConnectomeView import PHARYNX_VIEW as view
 
     print(
         "NOTE: For the sake of this paper, we excluded the pharyngeal neurons from the connectome data for both genders due to their distinction from the somatic nervous system."
@@ -128,26 +144,17 @@ def test_bilaterals():
     cds2 = cds.get_connectome_view(view)
 
     synclass = "Chemical Inh"
-    synclass = "Chemical Exc" if "Raw" not in view.name else "Chemical"
+    synclass = (
+        "Chemical Exc"
+        if "Raw" not in view.name
+        else ("Functional" if "Func" not in view.name else "Chemical")
+    )
 
-    # synclass = "Acetylcholine"
+
+    # print(cds2.connect)
+
     # synclass = "Chemical"
     # synclass = "Electrical"
-
-    print(cds2.summary())
-
-    print("Keys: %s, plotting: %s" % (view.synclass_sets.keys(), synclass))
-
-    # scaled_conn_array, info = convert_to_symmetry_array(cds2)
-
-    print("------")
-    # print(array_info(scaled_conn_array))
-
-    # amal = "CS+GJ"
-    # cds2.connections[amal] = scaled_conn_array
-
-    to_plot = "Electrical"
-    to_plot = "Chemical Exc"
 
     # import plotly.io as pio
     # pio.renderers.default = "browser"
@@ -155,7 +162,7 @@ def test_bilaterals():
         # cds2.connection_number_plot
 
         fig, info = cds2.to_plotly_matrix_fig(
-            to_plot, view, SYMMETRY_COLORMAP, bold_bilaterals=True, symmetry=True
+            synclass, view, SYMMETRY_COLORMAP, bold_bilaterals=True, symmetry=True
         )
 
         fig.show()
